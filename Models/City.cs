fbed4fa76e4bdb2c90a224b872ef2687e2f3b947
brishketukislay <brishketu.kislay@gmail.com>
2024-02-09 00:26:12 +0000

    appsetting update

--- Models/City.cs
+++ Models/City.cs
@@ -3,6 +3,6 @@ namespace webApi.Models
     public class City
     {
         public int id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
