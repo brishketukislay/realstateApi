bfabf409791fbbf8bf10bdda48e58bedf24812ea
brishketukislay <brishketu.kislay@gmail.com>
2024-03-10 01:43:38 +0000

    city

--- Controllers/cityController.cs
+++ Controllers/cityController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace webApi.Controllers
 {
-    [Route("api/[controller]")] // api/city
+    [Route("api/[controller]")] 
     [ApiController]
     public class cityController : ControllerBase  //inherite controllerbase
     {
