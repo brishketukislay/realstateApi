05f4f8fdb0f7c4df99df5cc7c95e0ab412944e73
brishketukislay <brishketu.kislay@gmail.com>
2024-03-11 00:57:54 +0000

    readonly string

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace webApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private static readonly string[] Summaries = new[] // readonly string
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
