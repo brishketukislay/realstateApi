7c8dfe329a39574c4e87d7543cca26b877cb60c0
brishketukislay <brishketu.kislay@gmail.com>
2024-03-13 23:05:36 +0000

    weather api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet(Name = "GetWeatherForecast")] //  GetWeatherForecast api
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
